f86b376688dbcb584cd7ad7b777222d47c693394
NicoCasas <nicolas.casas.250@gmail.com>
2019-10-31 15:16:33 -0300

    lpqtpbgit

--- TP_Final_0.0.1/src/TP_Final_0.0.1.c
+++ TP_Final_0.0.1/src/TP_Final_0.0.1.c
@@ -69,3 +69,44 @@ void confADCPin_0a3(uint8_t num){
 	}
 	return;
 }
+
+/*Configura como salidas pines contiguos pasados por parámteros
+ * Param:
+ * 			uint8_t puerto 	   Puerto a configurar. Debe ser un valor entre 0 y 3
+ * 			uint8_t primerBit  Primer pin a configurar. Debe ser un valor entre 0 y 32
+ * 			uint8_t ultimoBit  Ultimo bit a configurar. Debe ser un valor entre 0 y 32, y mayor a primerBit
+ *
+ */
+void confPinSal(uint8_t puerto,uint8_t primerBit, uint8_t ultimoBit){
+	uint32_t aux;
+	if(puerto<4&&primerBit<ultimoBit&&primerBit<32&&ultimoBit<32){
+		for(uint8_t i=primerBit;i<ultimoBit+1;i++){
+			aux+=(1<<i);
+		}
+		GPIO_SetDir(puerto,aux,SALIDA);
+	}
+	return;
+
+}
+
+/*Setea o limpia pines contiguos pasados por parámteros
+ * Param:
+ * 			uint8_t puerto 	   Puerto a configurar. Debe ser un valor entre 0 y 3
+ * 			uint8_t primerBit  Primer pin a configurar. Debe ser un valor entre 0 y 32
+ * 			uint8_t ultimoBit  Ultimo bit a configurar. Debe ser un valor entre 0 y 32, y mayor a primerBit
+ * 			uint8_t valor	   Limpia si valor=0, Setea si valor=1;
+ */
+void confPinSoC(uint8_t puerto,uint8_t primerBit, uint8_t ultimoBit,uint8_t valor){
+	uint32_t aux;
+	if(puerto<4&&primerBit<ultimoBit&&primerBit<32&&ultimoBit<32){		//Realiza controles
+
+		for(uint8_t i=primerBit;i<ultimoBit+1;i++){
+			aux+=(1<<i);
+		}
+
+		if		(valor==0)	GPIO_ClearValue(puerto,aux);
+		else if	(valor==1)	GPIO_SetValue(puerto,aux);
+	}
+	return;
+
+}
