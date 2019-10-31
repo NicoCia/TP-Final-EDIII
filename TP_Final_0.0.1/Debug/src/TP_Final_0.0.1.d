888599bdb82ea5fe82835340a43eb1d509a19bf8
NicoCasas <nicolas.casas.250@gmail.com>
2019-10-31 15:34:17 -0300

    holi

---
+++ TP_Final_0.0.1/Debug/src/TP_Final_0.0.1.d
@@ -0,0 +1,40 @@
+src/TP_Final_0.0.1.o src/TP_Final_0.0.1.d: ../src/TP_Final_0.0.1.c \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/lpc17xx.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_exti.h \
+ C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_exti.h:
+
+C:\Users\nico-\Documents\LPCXpresso_8.2.2_650\workspace2\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h:
