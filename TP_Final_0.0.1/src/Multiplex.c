388ce453958ea9c4dcd05c1b5ada0d3e49fa591b
NicoCia <nicocia7@gmail.com>
2019-11-10 15:54:38 -0300

    Multiple con problemas de asignacion de punto decimal

--- TP_Final_0.0.1/src/Multiplex.c
+++ TP_Final_0.0.1/src/Multiplex.c
@@ -11,44 +11,31 @@
 //para catodo comun-revisar
 uint8_t prender[]={0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110};
 uint8_t apagar[]={0b00000011, 0b10011111, 0b00100101, 0b00001101, 0b10011001, 0b01001001, 0b01000001, 0b00011111, 0b00000001, 0b00001001};
-
+uint8_t *nums[15]={};
 //uint16_t peso_max=5000/1024;
 
 /*Convierte el dato recibido en un valor unitario para mostrar por GPIO en 7 segmentos
  * Param:
  * 			uint16_t dato Valor a convertir
  */
-void convert(uint16_t dato, uint8_t *nums[], uint8_t i){
-	uint8_t bandera=1;
+void convert(uint16_t dato, Bloque i){
 	uint16_t resto=dato;//*peso_max;
 	uint8_t dig=i;
 
 	uint8_t parcial;
-	uint16_t div=1000;
-	static uint8_t punto;
-	static uint8_t llam;
+	uint16_t div=10000;
+	//static uint8_t punto;
 
-	punto=0;
-	llam=1;
-	while(bandera){
+	//punto=0;
+	while(div!=0){
 		parcial=resto/div;
 		resto=resto%div;
-		if(parcial<10){
-			nums[dig][1]=parcial;
-			nums[dig][2]=NO;
-			div=div/10;
-			dig++;
-		}
-		else {
-			llam++;
-			convert(parcial*div/10,nums,i);
-			nums[punto-1][2]=NO;
-			punto++;
-			dig=llam;
-		}
-		if(div==0)bandera=0;
+		nums[dig][1]=parcial;
+		nums[dig][2]=NO;
+		dig++;
+		div=div/10;
 	}
-	nums[punto][2]=SI;
+	//nums[punto][2]=SI;
 	return;
 }
 
--- TP_Final_0.0.1/src/Multiplex.h
+++ TP_Final_0.0.1/src/Multiplex.h
@@ -1,13 +1,7 @@
-/*
- * Multiplex.h
- *
- *  Created on: 31 de oct. de 2019
- *      Author: nicoc
- */
-
 #ifndef MULTIPLEX_H_
 #define MULTIPLEX_H_
 
 typedef enum {NO = 0, SI = !NO} Decimal;
+typedef enum {PRECIO = 0, PESO = 5, VALOR = 10, TECLADO = 10} Bloque;
 
 #endif /* MULTIPLEX_H_ */
