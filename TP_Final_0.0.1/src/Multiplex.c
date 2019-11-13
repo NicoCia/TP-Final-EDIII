8b043c7009bd501be03770576a45adb4102faa5d
NicoCia <nicocia7@gmail.com>
2019-11-13 02:11:14 -0300

    Multiplexacion y muestreo implementados

--- TP_Final_0.0.1/src/Multiplex.c
+++ TP_Final_0.0.1/src/Multiplex.c
@@ -12,6 +12,7 @@
 uint8_t prender[]={0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110};
 uint8_t apagar[]={0b00000011, 0b10011111, 0b00100101, 0b00001101, 0b10011001, 0b01001001, 0b01000001, 0b00011111, 0b00000001, 0b00001001};
 uint8_t *nums[15]={};
+uint8_t displayOff=0b00000000;
 
 /*Convierte el dato recibido en un valor unitario para mostrar por GPIO en 7 segmentos
  * Param:
@@ -31,21 +32,19 @@ void convert(uint16_t dato, Bloque i){
 
 	uint8_t parcial;
 	uint16_t div=10000;
-	uint8_t punto;
-
-	punto=0;
 
 	while(div!=0){
 		parcial=resto/div;
 		resto=resto%div;
-		nums[dig][1]=parcial;
-		nums[dig][2]=NO;
-		if(dig==2||dig==6||dig==12)punto=dig;
+		nums[dig][0]=parcial;
+		/*TODO
+		 * implementar que al estar en bloque TECLADO, los displays que no se utilizan en el codigo se apaguen
+		 if(i==TECLADO)*/
+		if((i!=TECLADO)&&(dig==2||dig==6||dig==12))nums[dig][1]=SI;
+		else nums[dig][1]=NO;
 		dig++;
 		div=div/10;
 	}
-
-	if(i!=TECLADO) nums[punto][2]=SI;
 
 	return;
 }
@@ -58,8 +57,8 @@ void convert(uint16_t dato, Bloque i){
 void display(uint32_t disp, uint8_t dig){
 	GPIO_ClearValue(0,disp);
 	GPIO_SetValue(0,~disp);
-	GPIO_ClearValue(0,(uint32_t)((apagar[nums[dig][1]]+(~nums[dig][2]))<<4));
-	GPIO_SetValue(0,(uint32_t)((prender[nums[dig][1]]+nums[dig][2])<<4));
+	GPIO_ClearValue(0,(uint32_t)((apagar[nums[dig][0]]+(~nums[dig][1]))<<4));
+	GPIO_SetValue(0,(uint32_t)((prender[nums[dig][0]]+nums[dig][1])<<4));
 
 	return;
 }
