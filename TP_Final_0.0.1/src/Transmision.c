099433048f11757782ba54163b89eff0f57761e3
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2019-11-25 20:07:12 -0300

    Trasmisión actualizada

--- TP_Final_0.0.1/src/Transmision.c
+++ TP_Final_0.0.1/src/Transmision.c
@@ -17,6 +17,9 @@ void confUART(void);
 void confUARTPin(void);
 void UART_IntTransmit(void);
 void UART0_IRQHandler(void);
+void enviarModo(void);
+void error(void);
+
 /*
  * Configura el Pin de Tx y Rx
  * Param:
@@ -55,7 +58,8 @@ void confUART(void){
 	//Habilita interrucpción Rx
 
 	UART_IntConfig(LPC_UART0, UART_INTCFG_RBR, ENABLE);
-
+	/* preemption = 1, sub-priority = 1 */
+	//NVIC_SetPriority(UART0_IRQn, ((0x01<<3)|0x01));
 	confUARTPin();
 	//Habilita interrpción por UART0
 	NVIC_EnableIRQ(UART0_IRQn);
@@ -71,7 +75,7 @@ void UART0_IRQHandler(void){
 	//Determina la fuente de interrupción
 	intsrc = UART_GetIntId(LPC_UART0);
 	tmp = intsrc & UART_IIR_INTID_MASK;
-	// Evalúa si Interrupt identification == Receive data available
+	// Evalúa si Transmit Holding está vacío
 	if (tmp == UART_IIR_INTID_RDA){
 		UART_IntTransmit();
 	}
@@ -83,9 +87,8 @@ void UART_IntTransmit(void){
 	uint32_t conta=0;
 	uint8_t mensaje[3]="";
 	uint8_t stock[1500]="";
-	getStockEnAscii(stock);
-	//uint8_t error[]={"\r\nIngrese 's' para obtener el stock\n\r"};
 	uint8_t bandera=0;
+	uint8_t mensaje1[]="Codigo\tPrecio\tCantidad\n\r";
 
 	UART_Receive(LPC_UART0, mensaje, 1, BLOCKING);
 	mensaje[1]='\n'; mensaje[2]='\r';
@@ -108,20 +111,44 @@ void UART_IntTransmit(void){
 		case'D': bandera=0; accion(13); break;
 		case'#': bandera=0; accion(14); break;
 		case'*': bandera=0; accion(15); break;
-		case'S': bandera=1; break;
+		case'M': bandera=0; enviarModo(); break;
+		case'S': bandera=1; getStockEnAscii(stock);break;
+		default: bandera=0; error(); break;
+	}
+	if(bandera){
+		for(uint16_t i=0; i<15000; i++){
+			if(stock[i]=='z') break;
+			conta++;
+		}
+		UART_Send(LPC_UART0, mensaje1, sizeof(mensaje1), BLOCKING);
+		UART_Send(LPC_UART0, stock, conta, BLOCKING);
+	}
+	else sendPrueba();
+
+	return;
+}
+
+void enviarModo(void){
+	uint8_t aux[]="MODO = PESANDO\n\n\r";
+	uint8_t aux1[]="MODO = MANUAL\n\n\r";
+	uint8_t aux2[]="MODO = PRECONFIGURADO\n\n\r";
+	uint8_t aux3[]="MODO = SUMA PARCIAL\n\n\r";
+	uint8_t aux4[]="MODO = MODIFICAR DATOS\n\n\r";
+
+	switch(getModo()){
+		case 0: UART_Send(LPC_UART0, aux, sizeof(aux), BLOCKING); break;
+		case 1: UART_Send(LPC_UART0, aux1, sizeof(aux1), BLOCKING);break;
+		case 2: UART_Send(LPC_UART0, aux2, sizeof(aux2), BLOCKING); break;
+		case 3: UART_Send(LPC_UART0, aux3, sizeof(aux3), BLOCKING); break;
+		case 4: UART_Send(LPC_UART0, aux4, sizeof(aux4), BLOCKING); break;
 		default: break;
 	}
+	return;
+}
 
-	for(uint16_t i=0; i<15000; i++){
-		if(stock[i]=='z') break;
-		conta++;
-	}
-	if(bandera) UART_Send(LPC_UART0, stock, conta, BLOCKING);
-	else sendPrueba();
-	/*else {
-		UART_Send(LPC_UART0, error, sizeof(error), BLOCKING);
-		UART_IntConfig(LPC_UART0, UART_INTCFG_THRE, ENABLE);
-	}*/
+void error(void){
+	uint8_t error[]="La tecla presionada no es valida.\n\rLas teclas posibles son:\n\r0-9: numeros\n\rA: Modo Manual\n\rB: Modo Preconfigurado\n\rC: Modo Carga de Precios\n\rD: ENTER\n\r#: Cancelar\n\r*: Tarar\n\rM: Muestra Modo Actual\n\rS: Imprime Stock\n\r";//Q: Muestra el Precio por Display\n\rW: Muestra el Peso por display\n\rE: Muestra el Monto por Display\n\r";
+	UART_Send(LPC_UART0, error, sizeof(error), BLOCKING);
 
 	return;
 }
